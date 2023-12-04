7cbeb624a4b12edd984a9fa4f54d488be5ed7b72
Igor Soengas <isoengas@gmail.com>
2023-12-04 10:17:48 +0100

    Day 1 solved

---
+++ 2023/Day1/Program.cs
@@ -0,0 +1,49 @@
+﻿namespace Day1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Day 1 - Trebuchet?!");
+            var lines = File.ReadAllLines("input.txt");
+            Console.WriteLine("Part 1: " + lines.Select(ParseLine).Sum());
+            Console.WriteLine("Part 2: " + lines.Select(ParseLineWithWords).Sum());
+        }
+
+        private static Dictionary<string, string> replaces = new() {
+            ["one"] = "1",
+            ["two"] = "2",
+            ["three"] = "3",
+            ["four"] = "4",
+            ["five"] = "5",
+            ["six"] = "6",
+            ["seven"] = "7",
+            ["eight"] = "8",
+            ["nine"] = "9"
+        };
+
+        public static int ParseLineWithWords(string line)
+        {
+            var mins = replaces
+                .Select(r => (r.Key, line.IndexOf(r.Key)))
+                .Where(i => i.Item2 >= 0)
+                .OrderBy(q => q.Item2);
+            if (mins.Any())
+                line = line.Substring(0, mins.First().Item2) + replaces[mins.First().Key] + line.Substring(mins.First().Item2);
+
+            var maxs = replaces
+                .Select(r => (r.Key, line.LastIndexOf(r.Key)))
+                .Where(i => i.Item2 >= 0)
+                .OrderByDescending(q => q.Item2);
+            if (maxs.Any())
+                line = line.Substring(0, maxs.First().Item2 + maxs.First().Key.Length) + replaces[maxs.First().Key] + line.Substring(maxs.First().Item2 + maxs.First().Key.Length);
+
+            return ParseLine(line);
+        }
+
+        public static int ParseLine(string line)
+        {
+            return (line.First(char.IsDigit) - '0') * 10 + (line.Last(char.IsDigit) - '0');
+        }
+    }
+}
---
+++ 2023/Day1/Tests.cs
@@ -0,0 +1,27 @@
+﻿using NUnit.Framework;
+
+namespace Day1
+{
+    [TestFixture]
+    internal class Tests
+    {
+        [Test]
+        [TestCase("1abc2", ExpectedResult = 12)]
+        [TestCase("pqr3stu8vwx", ExpectedResult = 38)]
+        [TestCase("a1b2c3d4e5f", ExpectedResult = 15)]
+        [TestCase("treb7uchet", ExpectedResult = 77)]
+        [TestCase("two1nine", ExpectedResult = 29)]
+        [TestCase("eightwothree", ExpectedResult = 83)]
+        [TestCase("abcone2threexyz", ExpectedResult = 13)]
+        [TestCase("xtwone3four", ExpectedResult = 24)]
+        [TestCase("4nineeightseven2", ExpectedResult = 42)]
+        [TestCase("zoneight234", ExpectedResult = 14)]
+        [TestCase("2svpbhrlhfjhbkf3fourvvspkfmbvztmtpcxndfnine9", ExpectedResult = 29)]
+        [TestCase("one", ExpectedResult = 11)]
+        [TestCase("oneight", ExpectedResult = 18)]
+        public int TestParseLine(string line)
+        {
+            return Program.ParseLineWithWords(line);
+        }
+    }
+}
