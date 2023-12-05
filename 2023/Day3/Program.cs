9218fee9969dce264bc885d53b3a9d60aa2803a2
Igor Soengas <isoengas@gmail.com>
2023-12-05 12:14:20 +0100

    Day 3 solved

---
+++ 2023/Day3/Program.cs
@@ -0,0 +1,143 @@
+﻿using System.Drawing;
+
+namespace Day3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Day 3: Gear Ratios");
+            var input = File.ReadAllLines("input.txt");
+            List<int> parts = ExtractParts(input);
+            Console.WriteLine("Part 1: " + parts.Sum());
+            List<int> gears = ExtractGears(input);
+            Console.WriteLine("Part 2: " + gears.Sum());
+        }
+
+        internal static List<int> ExtractGears(string[] input)
+        {
+            HashSet<Symbol> gears = new HashSet<Symbol>(ExtractSymbols(input).Where(s => s.IsGear));
+            HashSet<(int, Symbol)> partGearPairs = [];
+            int y = 0;
+            foreach (string line in input)
+            {
+                int x = 0;
+                while (x < line.Length)
+                {
+                    int? currentNumber = null;
+                    bool isPart = false;
+                    HashSet<Symbol> adjacentGears = [];
+                    while (x < line.Length && char.IsDigit(line[x]))
+                    {
+                        currentNumber = currentNumber.GetValueOrDefault() * 10 + line[x] - '0';
+                        foreach (var gear in GetAdjacentSymbols(x, y, gears))
+                            adjacentGears.Add(gear);
+                        x++;
+                    }
+                    if (currentNumber.HasValue)
+                    {
+                        foreach (var adjacentGear in adjacentGears)
+                            partGearPairs.Add((currentNumber.Value, adjacentGear));
+                        adjacentGears.Clear();
+                        currentNumber = null;
+                        isPart = false;
+                    }
+                    else
+                    {
+                        x++;
+                    }
+                }
+                y++;
+            }
+
+            return partGearPairs
+                    .GroupBy(p => p.Item2)
+                    .Where(g => g.Count() == 2)
+                    .Select(g => g.First().Item1 * g.Last().Item1)
+                    .ToList();
+        }
+
+        internal static List<int> ExtractParts(string[] input)
+        {
+            HashSet<Symbol> gears = new HashSet<Symbol>(ExtractSymbols(input));
+            List<int> parts = [];
+            int y = 0;
+            foreach (string line in input)
+            {
+                int x = 0;
+                while (x < line.Length)
+                {
+                    int? currentNumber = null;
+                    bool isPart = false;
+                    List<Symbol> adjacentGears = [];
+                    while (x < line.Length && char.IsDigit(line[x]))
+                    {
+                        currentNumber = currentNumber.GetValueOrDefault() * 10 + line[x] - '0';
+                        adjacentGears.AddRange(GetAdjacentSymbols(x, y, gears));
+                        isPart = isPart || GetAdjacentSymbols(x, y, gears).Any();
+                        x++;
+                    }
+                    if (currentNumber.HasValue && isPart)
+                    {
+                        parts.Add(currentNumber.Value);
+                        currentNumber = null;
+                        isPart = false;
+                    }
+                    else
+                    {
+                        x++;
+                    }
+                }
+                y++;
+            }
+
+            return parts;
+        }
+
+        private static IEnumerable<Symbol> GetAdjacentSymbols(int x, int y, HashSet<Symbol> symbols)
+        {
+            List<Point> adjacentCells = [
+                 new(x - 1, y - 1),
+                new(x, y - 1),
+                new(x + 1, y - 1),
+                new(x - 1, y),
+                new(x + 1, y),
+                new(x - 1, y + 1),
+                new(x, y + 1),
+                new(x + 1, y + 1)
+             ];
+            return symbols.Where(s => adjacentCells.Contains(s.location));
+        }
+
+        internal static IEnumerable<Symbol> ExtractSymbols(string[] input)
+        {
+            int y = 0;
+            foreach (var line in input)
+            {
+                foreach (var symbol in ExtractSymbols(line, y))
+                {
+                    yield return symbol;
+                }
+                y++;
+            }
+        }
+
+        private static IEnumerable<Symbol> ExtractSymbols(string line, int y)
+        {
+            return line
+                    .Select((chr, x) => new { chr, x })
+                    .Where(p => IsSymbol(p.chr))
+                    .Select(s => new Symbol(new Point(s.x, y), s.chr));
+        }
+
+        private static bool IsSymbol(char chr)
+        {
+            return !char.IsDigit(chr) && chr != '.';
+        }
+    }
+
+    internal record Symbol(Point location, char c)
+    {
+        public bool IsGear => c == '*';
+    }
+}
---
+++ 2023/Day3/Tests.cs
@@ -0,0 +1,77 @@
+﻿using NUnit.Framework;
+using System.Drawing;
+
+namespace Day3
+{
+    [TestFixture]
+    public class Tests
+    {
+        [Test]
+        public void Test_symbol_extraction()
+        {
+            string[] lines = [
+                "467..114..",
+                "...*......",
+                "..35..633.",
+                "......#...",
+                "617*......",
+                ".....+.58.",
+                "..592.....",
+                "......755.",
+                "...$.*....",
+                ".664.598.."
+            ];
+            var expectedSymbols = new List<Symbol>
+            {
+                new(new(3, 1), '*'),
+                new(new(6, 3), '#'),
+                new(new(3, 4), '*'),
+                new(new(5, 5), '+'),
+                new(new(3, 8), '$'),
+                new(new(5, 8), '*')
+            };
+            var symbols = Program.ExtractSymbols(lines).ToList();
+            Assert.That(symbols, Is.EqualTo(expectedSymbols));
+        }
+
+        [Test]
+        public void Test_part_extraction()
+        {
+            string[] lines = [
+                "467..114..",
+                "...*......",
+                "..35..633.",
+                "......#...",
+                "617*......",
+                ".....+.58.",
+                "..592.....",
+                "......755.",
+                "...$.*....",
+                ".664.598.."
+            ];
+            List<int> expectedParts = [467, 35, 633, 617, 592, 755, 664, 598];
+            var parts = Program.ExtractParts(lines);
+            Assert.That(parts, Is.EqualTo(expectedParts));
+        }
+
+        [Test]
+        public void Test_gear_extraction()
+        {
+            string[] lines = [
+                "467..114..",
+                "...*......",
+                "..35..633.",
+                "......#...",
+                "617*......",
+                ".....+.58.",
+                "..592.....",
+                "......755.",
+                "...$.*....",
+                ".664.598.."
+            ];
+            List<int> expectedGears = [16345, 451490];
+            var gears = Program.ExtractGears(lines);
+            Assert.That(gears, Is.EqualTo(expectedGears));
+        }
+    }
+}
