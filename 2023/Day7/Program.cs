08260abbd057b96294ee1967e8cfb771e7e774aa
Igor Soengas <isoengas@gmail.com>
2023-12-11 10:48:27 +0100

    Day 7 complete

--- 2023/Day7/Program.cs
+++ 2023/Day7/Program.cs
@@ -4,7 +4,101 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Day 7: Camel Cards");
+            
+            var gameRecords = File.ReadLines("input.txt");
+            
+            var games = gameRecords
+                            .Select(r => new Game(new Hand(r.Split(' ')[0]), int.Parse(r.Split(' ')[1])));
+            var winnings = games.OrderBy(h => h.hand).Select((game, index) => (long)(index + 1) * game.bid).Sum();
+            Console.WriteLine("Part 1: " + winnings);
+            
+            var gamesWithJoker = gameRecords
+                            .Select(r => new GameWithJoker(new HandWithJoker(r.Split(' ')[0]), int.Parse(r.Split(' ')[1])));
+            winnings = gamesWithJoker.OrderBy(h => h.hand).Select((game, index) => (long)(index + 1) * game.bid).Sum();
+            Console.WriteLine("Part 2: " + winnings);
         }
     }
+
+    public record Hand(string cards) : IComparable<Hand>
+    {
+        private const string Strength = "23456789TJQKA";
+
+        public string Cards => cards;
+
+        public int GetHandWeight()
+        {
+            return cards
+                    .GroupBy(c => c)
+                    .Select(g => g.Count())
+                    .Where(c => c > 1)
+                    .OrderByDescending(c => c)
+                    .Select((num, index) => new { num, index })
+                    .Aggregate(0, (acc, numIndex) => (int)(acc + (numIndex.num * Math.Pow(10, (1 - numIndex.index)))));
+        }
+
+        public int CompareTo(Hand other)
+        {
+            int xWeight = GetHandWeight();
+            int yWeight = other.GetHandWeight();
+            if (xWeight != yWeight) return xWeight - yWeight;
+            int i = 0;
+            while (i < 5)
+            {
+                int strX = Strength.IndexOf(Cards[i]);
+                int strY = Strength.IndexOf(other.Cards[i]);
+                if (strX != strY) return strX - strY;
+                i++;
+            }
+            return 0;
+        }
+    }
+
+    public record HandWithJoker(string cards) : IComparable<HandWithJoker>
+    {
+        private const string Strength = "J23456789TQKA";
+
+        public string Cards => cards;
+
+        public int GetHandWeight()
+        {
+            var weightWithoutJokers = Cards
+                    .Where(c => c != 'J')
+                    .GroupBy(c => c)
+                    .Select(g => g.Count())
+                    .Where(c => c > 1)
+                    .OrderByDescending(c => c)
+                    .Select((num, index) => new { num, index })
+                    .Aggregate(0, (acc, numIndex) => (int)(acc + (numIndex.num * Math.Pow(10, (1 - numIndex.index)))));
+            var numJokers = Cards.Count(c => c == 'J');
+            return numJokers switch
+            {
+                0 => weightWithoutJokers,
+                1 => weightWithoutJokers > 0 ? weightWithoutJokers + 10 : 20,
+                2 => weightWithoutJokers > 0 ? weightWithoutJokers + 20 : 30,
+                3 => weightWithoutJokers > 0 ? 50 : 40,
+                4 => 50,
+                5 => 50
+            };
+        }
+
+        public int CompareTo(HandWithJoker other)
+        {
+            int xWeight = GetHandWeight();
+            int yWeight = other.GetHandWeight();
+            if (xWeight != yWeight) return xWeight - yWeight;
+            int i = 0;
+            while (i < 5)
+            {
+                int strX = Strength.IndexOf(Cards[i]);
+                int strY = Strength.IndexOf(other.Cards[i]);
+                if (strX != strY) return strX - strY;
+                i++;
+            }
+            return 0;
+        }
+    }
+
+    public record Game(Hand hand, int bid);
+    public record GameWithJoker(HandWithJoker hand, int bid);
 }
---
+++ 2023/Day7/Tests.cs
@@ -0,0 +1,106 @@
+﻿using NUnit.Framework;
+
+namespace Day7
+{
+    [TestFixture]
+    public class Tests
+    {
+        [Test]
+        [TestCase("32T3K", ExpectedResult = 20)]
+        [TestCase("KK677", ExpectedResult = 22)]
+        [TestCase("T55J5", ExpectedResult = 30)]
+        [TestCase("23456", ExpectedResult = 0)]
+        [TestCase("23332", ExpectedResult = 32)]
+        [TestCase("AA8AA", ExpectedResult = 40)]
+        [TestCase("AAAAA", ExpectedResult = 50)]
+        public int TestHandWeight(string cards)
+        {
+            return new Hand(cards).GetHandWeight();
+        }
+
+        [Test]
+        [TestCase("2345J", ExpectedResult = 20)]
+        [TestCase("KKJ77", ExpectedResult = 32)]
+        [TestCase("T55J5", ExpectedResult = 40)]
+        [TestCase("23J56", ExpectedResult = 20)]
+        [TestCase("J3332", ExpectedResult = 40)]
+        [TestCase("AAJAA", ExpectedResult = 50)]
+        [TestCase("JJJJJ", ExpectedResult = 50)]
+        [TestCase("JJJ77", ExpectedResult = 50)]
+        [TestCase("JJJ67", ExpectedResult = 40)]
+        [TestCase("JJ667", ExpectedResult = 40)]
+        [TestCase("JJ666", ExpectedResult = 50)]
+        [TestCase("JJ678", ExpectedResult = 30)]
+        public int TestHandWeightWithJokers(string cards)
+        {
+            return new HandWithJoker(cards).GetHandWeight();
+        }
+
+        [Test]
+        [TestCase("KK677", "KTJJT", ExpectedResult = 1)]
+        public int TestHandComparer(string hand1, string hand2)
+        {
+            var h1 = new Hand(hand1);
+            var h2 = new Hand(hand2);
+            return h1.CompareTo(h2) switch
+            {
+                0 => 0,
+                > 0 => 1,
+                < 0 => -1
+            };
+        }
+
+        [Test]
+        public void TestSort()
+        {
+            var hands = new List<Hand>
+            {
+                new("32T3K"),
+                new("T55J5"),
+                new("KK677"),
+                new("KTJJT"),
+                new("QQQJA")
+            };
+            var sorted = hands.OrderBy(h => h).ToList();
+            var expectedResult = new List<Hand>
+            {
+                new("32T3K"),
+                new("KTJJT"),
+                new("KK677"),
+                new("T55J5"),
+                new("QQQJA")
+            };
+            Assert.That(sorted, Is.EqualTo(expectedResult));
+        }
+
+        [Test]
+        public void TestWinnings()
+        {
+            var games = new List<Game>
+            {
+                new(new("32T3K"), 765),
+                new(new("T55J5"), 684),
+                new(new("KK677"), 28),
+                new(new("KTJJT"), 220),
+                new(new("QQQJA"), 483)
+            };
+            var winnings = games.OrderBy(h => h.hand).Select((game, index) => (index + 1) * game.bid).Sum();
+            Assert.That(winnings, Is.EqualTo(6440));
+        }
+
+        [Test]
+        public void TestWinningsWithJoker()
+        {
+            var games = new List<GameWithJoker>
+            {
+                new(new("32T3K"), 765),
+                new(new("T55J5"), 684),
+                new(new("KK677"), 28),
+                new(new("KTJJT"), 220),
+                new(new("QQQJA"), 483)
+            };
+            var winnings = games.OrderBy(h => h.hand).Select((game, index) => (index + 1) * game.bid).Sum();
+            Assert.That(winnings, Is.EqualTo(5905));
+        }
+    }
+}
