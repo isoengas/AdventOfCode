5b0583e410472139fa706bc1fd8d62410cb93b43
Igor Soengas <isoengas@gmail.com>
2023-12-18 10:10:19 +0100

    Day 15 finished

---
+++ 2023/Day15/Program.cs
@@ -0,0 +1,81 @@
+﻿
+namespace Day15
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Day 15: Lens Library");
+            string line = File.ReadAllText("input.txt");
+            var sum = line.Split(',').Select(GetHash).Sum();
+            Console.WriteLine("Part 1: " + sum);
+            sum = InitSequence(line);
+            Console.WriteLine("Part 2: " + sum);
+        }
+
+        public static int InitSequence(string line)
+        {
+            var boxes = Enumerable.Range(1, 256).Select(i => new Box(i)).ToArray();
+            foreach (var instr in line.Split(','))
+            {
+                if (instr.Contains('-'))
+                    RemoveLens(boxes, instr.Split('-')[0]);
+                else if (instr.Contains('='))
+                    PutLens(boxes, instr.Split('=')[0], int.Parse(instr.Split('=')[1]));
+            }
+            return boxes.Select(b => b.FocusingPower).Sum();
+        }
+
+        private static void RemoveLens(Box[] boxes, string label)
+        {
+            int boxNumber = GetHash(label);
+            var lens = boxes[boxNumber].GetLens(label);
+            if (lens is not null)
+            {
+                boxes[boxNumber].Lenses.Remove(lens);
+            }
+        }
+
+        private static void PutLens(Box[] boxes, string label, int focalLength)
+        {
+            int boxNumber = GetHash(label);
+            var lens = boxes[boxNumber].GetLens(label);
+            if (lens is not null)
+            {
+                lens.FocalLength = focalLength;
+            }
+            else
+            {
+                boxes[boxNumber].Lenses.Add(new Lens(label, focalLength));
+            }
+        }
+
+        public static int GetHash(string s)
+        {
+            int curr = 0;
+            foreach (var c in s)
+            {
+                curr += c;
+                curr *= 17;
+                curr %= 256;
+            }
+            return curr;
+        }
+    }
+
+    public class Box(int boxNumber)
+    {
+        public int BoxNumber => boxNumber;
+        public List<Lens> Lenses { get; } = [];
+
+        public int FocusingPower => Lenses.Select((l, i) => l.FocalLength * (i + 1) * BoxNumber).Sum();
+
+        public Lens? GetLens(string label) => Lenses.FirstOrDefault(l => l.Label == label);
+    }
+
+    public class Lens(string label, int focalLength)
+    {
+        public string Label => label;
+        public int FocalLength { get; set; } = focalLength;
+    }
+}
---
+++ 2023/Day15/Tests.cs
@@ -0,0 +1,32 @@
+﻿using NUnit.Framework;
+
+namespace Day15
+{
+    [TestFixture]
+    public class Tests
+    {
+        [Test]
+        [TestCase("rn=1", ExpectedResult = 30)]
+        [TestCase("cm-", ExpectedResult = 253)]
+        [TestCase("qp=3", ExpectedResult = 97)]
+        [TestCase("cm=2", ExpectedResult = 47)]
+        [TestCase("qp-", ExpectedResult = 14)]
+        [TestCase("pc=4", ExpectedResult = 180)]
+        [TestCase("ot=9", ExpectedResult = 9)]
+        [TestCase("ab=5", ExpectedResult = 197)]
+        [TestCase("pc-", ExpectedResult = 48)]
+        [TestCase("pc=6", ExpectedResult = 214)]
+        [TestCase("ot=7", ExpectedResult = 231)]
+        public int Test_Hash(string input)
+        {
+            return Program.GetHash(input);
+        }
+
+        [Test]
+        public void Test_InitSequence()
+        {
+            var initSeq = "rn=1,cm-,qp=3,cm=2,qp-,pc=4,ot=9,ab=5,pc-,pc=6,ot=7";
+            Assert.That(Program.InitSequence(initSeq), Is.EqualTo(145));
+        }
+    }
+}
