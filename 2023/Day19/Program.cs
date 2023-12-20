2025dd25979d2d20ee9ff71d42d285a02d9c4240
Igor Soengas <isoengas@gmail.com>
2023-12-20 10:48:03 +0100

    Day 19 part 1 solved

---
+++ 2023/Day19/Program.cs
@@ -0,0 +1,179 @@
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace Day19
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Day 19: Aplenty");
+            var inputWf = File.ReadAllLines("input_wf.txt");
+            var inputParts = File.ReadAllLines("input_parts.txt");
+            int result = ProcessParts(inputWf, inputParts);
+            Console.WriteLine("Part 1: " + result);
+        }
+
+        public static int ProcessParts(string[] inputWf, string[] inputParts)
+        {
+            Dictionary<string, Workflow> workflows = inputWf.Select(w => Workflow.Parse(w)).ToDictionary(w => w.Name);
+            List<Part> parts = inputParts.Select(p => Part.Parse(p)).ToList();
+            foreach (var part in parts)
+            {
+                Workflow next = workflows["in"];
+                while (true)
+                {
+                    var result = next.Run(part);
+                    if (result == "A")
+                    {
+                        part.IsAccepted = true;
+                        break;
+                    }
+                    else if (result == "R")
+                    {
+                        break;
+                    }
+                    else
+                    {
+                        next = workflows[result];
+                    }
+                }
+            }
+            return parts.Where(p => p.IsAccepted).Sum(p => p.X + p.M + p.A + p.S);
+        }
+    }
+
+    public class Workflow
+    {
+        public Workflow(string name, Rule[] rules)
+        {
+            Name = name;
+            _rules = rules;
+        }
+        public string Name { get; }
+        private Rule[] _rules;
+
+        public static Workflow Parse(string instructions)
+        {
+            return new Workflow(instructions.Split('{')[0], instructions.Split('{')[1].Split('}')[0].Split(',').Select(r => Rule.Parse(r)).ToArray());
+        }
+
+        public string Run(Part part)
+        {
+            return _rules.Select(r => r.Run(part)).First(dest => dest != null);
+        }
+    }
+
+    public class Rule
+    {
+        private Rule(Condition condition, string destination)
+        {
+            Condition = condition;
+            Destination = destination;
+        }
+        public static Rule Parse(string rule)
+        {
+            if (rule.Contains(':'))
+            {
+                return new Rule(Condition.Parse(rule.Split(':')[0]), rule.Split(':')[1]);
+            }
+            else
+            {
+                return new Rule(Condition.True(), rule);
+            }
+        }
+
+        public string? Run(Part part)
+        {
+            if (Condition.Check(part))
+                return Destination;
+            return null;
+        }
+        private Condition Condition { get; set; }
+        private string Destination { get; set; }
+    }
+
+    public class Condition
+    {
+        private static Condition _true = new Condition { Check = _ => true };
+        public static Condition Parse(string condition)
+        {
+            var c = new Condition();
+            if (condition.Contains("<"))
+            {
+                int value = int.Parse(condition.Split('<')[1]);
+                if (condition[0] == 'x')
+                {
+                    c.Check = p => p.X < value;
+                }
+                else if (condition[0] == 'm')
+                {
+                    c.Check = p => p.M < value;
+                }
+                else if (condition[0] == 'a')
+                {
+                    c.Check = p => p.A < value;
+                }
+                else
+                {
+                    c.Check = p => p.S < value;
+                }
+            }
+            else if (condition.Contains(">"))
+            {
+                int value = int.Parse(condition.Split('>')[1]);
+                if (condition[0] == 'x')
+                {
+                    c.Check = p => p.X > value;
+                }
+                else if (condition[0] == 'm')
+                {
+                    c.Check = p => p.M > value;
+                }
+                else if (condition[0] == 'a')
+                {
+                    c.Check = p => p.A > value;
+                }
+                else
+                {
+                    c.Check = p => p.S > value;
+                }
+            }
+            else
+            {
+                throw new InvalidOperationException("Unexpected condition: " + condition);
+            }
+            return c;
+        }
+
+        public static Condition True() => _true;
+
+        public Predicate<Part> Check { get; private set; } = (_) => true;
+    }
+
+    public class Part
+    {
+        public Part(int x, int m, int a, int s)
+        {
+            X = x;
+            M = m;
+            A = a;
+            S = s;
+        }
+        public static Part Parse(string part)
+        {
+            var p = part.TrimStart('{').TrimEnd('}');
+            var ps = p.Split(',');
+            int x = int.Parse(ps[0].Split('=')[1]);
+            int m = int.Parse(ps[1].Split('=')[1]);
+            int a = int.Parse(ps[2].Split('=')[1]);
+            int s = int.Parse(ps[3].Split('=')[1]);
+            return new Part(x, m, a, s);
+        }
+        public int X { get; }
+        public int M { get; }
+        public int A { get; }
+        public int S { get; }
+
+        public bool IsAccepted { get; set; }
+    }
+}
---
+++ 2023/Day19/Tests.cs
@@ -0,0 +1,47 @@
+﻿using NUnit.Framework;
+
+namespace Day19
+{
+    [TestFixture]
+    public class Tests
+    {
+        [Test]
+        [TestCase("in{s<1351:px,qqz}", "{x=787,m=2655,a=1222,s=2876}", ExpectedResult = "qqz")]
+        [TestCase("qqz{s>2770:qs,m<1801:hdj,R}", "{x=787,m=2655,a=1222,s=2876}", ExpectedResult = "qs")]
+        [TestCase("qs{s>3448:A,lnx}", "{x=787,m=2655,a=1222,s=2876}", ExpectedResult = "lnx")]
+        [TestCase("lnx{m>1548:A,A}", "{x=787,m=2655,a=1222,s=2876}", ExpectedResult = "A")]
+        public string Test_Workflow(string wf, string p)
+        {
+            var workflow = Workflow.Parse(wf);
+            var part = Part.Parse(p);
+            return workflow.Run(part);
+        }
+
+        [Test]
+        public void Test_Part1_Example()
+        {
+            string[] parts = [
+                "{x=787,m=2655,a=1222,s=2876}",
+                "{x=1679,m=44,a=2067,s=496}",
+                "{x=2036,m=264,a=79,s=2244}",
+                "{x=2461,m=1339,a=466,s=291}",
+                "{x=2127,m=1623,a=2188,s=1013}"
+            ];
+            string[] workflows = [
+                "px{a<2006:qkq,m>2090:A,rfg}",
+                "pv{a>1716:R,A}",
+                "lnx{m>1548:A,A}",
+                "rfg{s<537:gd,x>2440:R,A}",
+                "qs{s>3448:A,lnx}",
+                "qkq{x<1416:A,crn}",
+                "crn{x>2662:A,R}",
+                "in{s<1351:px,qqz}",
+                "qqz{s>2770:qs,m<1801:hdj,R}",
+                "gd{a>3333:R,R}",
+                "hdj{m>838:A,pv}",
+            ];
+            Assert.That(Program.ProcessParts(workflows, parts), Is.EqualTo(19114));
+        }
+
+    }
+}
