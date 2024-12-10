86022432d850f4dd8667ae3d616b2f86019cb839
Igor Soengas <isoengas@gmail.com>
2024-12-10 11:43:33 +0100

    2024 day 1 and 2

---
+++ 2024/Day1/Program.cs
@@ -0,0 +1,35 @@
+﻿using System.Linq;
+using System.Runtime.CompilerServices;
+
+namespace Day1
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Day 1 - Historian Hysteria");
+            var lines = File.ReadAllLines("input.txt");
+            var (list1, list2) = Parse(lines);
+            list1.Sort();
+            list2.Sort();
+            var result = list1.Zip(list2, (i1, i2) => Math.Abs(i1 - i2)).Sum();
+            Console.WriteLine("Part 1: " + result);
+            var times = list2.GroupBy(n => n).ToDictionary(g => g.Key, g => g.Count());
+            var simmilarity = list1.Aggregate(0, (sum, n) => sum + (n * (times.TryGetValue(n, out var m) ? m : 0)));
+            Console.WriteLine("Part 1: " + simmilarity);
+        }
+
+        private static (List<int> list1, List<int> list2) Parse(string[] lines)
+        {
+            List<int> l1 = new();
+            List<int> l2 = new();
+
+            foreach (var line in lines)
+            {
+                l1.Add(int.Parse(line.Split(' ', StringSplitOptions.RemoveEmptyEntries)[0]));
+                l2.Add(int.Parse(line.Split(' ', StringSplitOptions.RemoveEmptyEntries)[1]));
+            }
+            return (l1, l2);
+        }
+    }
+}
---
+++ 2024/Day2/Program.cs
@@ -0,0 +1,60 @@
+﻿
+namespace Day2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Day 1 - Red-Nosed Reports");
+            var lines = File.ReadAllLines("input.txt");
+            var safeReports = lines.Select(Parse).Count(rep => IsSafe(rep));
+            Console.WriteLine("Part 1: " + safeReports);
+            var safeReportsWithDampener = lines.Select(Parse).Count(rep => IsSafe(rep, true));
+            Console.WriteLine("Part 2: " + safeReportsWithDampener);
+        }
+
+        private static bool IsSafe(IEnumerable<int> report, bool useDampener = false)
+        {
+            var diffs = report.Zip(report.Skip(1), (a, b) => b - a);
+            bool? inc = null;
+            foreach (var (index, diff) in diffs.Index())
+            {
+                if (diff == 0 || diff < -3 || diff > 3)
+                {
+                    var withoutPrevious = report.Take(index - 1).Concat(report.Skip(index));
+                    var withoutCurrent = report.Take(index).Concat(report.Skip(index + 1));
+                    var withoutNext = report.Take(index + 1).Concat(report.Skip(index + 2));
+                    return useDampener && (IsSafe(withoutPrevious) || IsSafe(withoutCurrent) || IsSafe(withoutNext));
+                }
+                if (diff < 0)
+                {
+                    if (inc == true)
+                    {
+                        var withoutPrevious = report.Take(index - 1).Concat(report.Skip(index));
+                        var withoutCurrent = report.Take(index).Concat(report.Skip(index + 1));
+                        var withoutNext = report.Take(index + 1).Concat(report.Skip(index + 2));
+                        return useDampener && (IsSafe(withoutPrevious) || IsSafe(withoutCurrent) || IsSafe(withoutNext));
+                    }
+                    inc = false;
+                }
+                else
+                {
+                    if (inc == false)
+                    {
+                        var withoutPrevious = report.Take(index - 1).Concat(report.Skip(index));
+                        var withoutCurrent = report.Take(index).Concat(report.Skip(index + 1));
+                        var withoutNext = report.Take(index + 1).Concat(report.Skip(index + 2));
+                        return useDampener && (IsSafe(withoutPrevious) || IsSafe(withoutCurrent) || IsSafe(withoutNext));
+                    }
+                    inc = true;
+                }
+            }
+            return true;
+        }
+
+        private static int[] Parse(string line)
+        {
+            return line.Split(' ', StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+        }
+    }
+}
